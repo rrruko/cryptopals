316114e0b9296b84af30afa53ef3e77b5d587134
ruko <rukokarasu@gmail.com>
2018-01-21 10:33:22 -0800

    Write procedure to demonstrate challenge 9 solution

--- src/set_2.rs
+++ src/set_2.rs
@@ -1,3 +1,10 @@
-pub fn set_2() {
+use pkcs::*;
 
+pub fn set_2() {
+    _9();
+}
+
+fn _9() {
+    let x = b"YELLOW SUBMARINE";
+    assert_eq!(b"YELLOW SUBMARINE\x04\x04\x04\x04", &pkcs7(x, 20).unwrap()[..]);
 }
