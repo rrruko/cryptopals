fe4ceb202dd6a9126f10a283771cebc4c0547308
ruko <rukokarasu@gmail.com>
2018-01-21 11:59:19 -0800

    Simplify pkcs7

--- src/pkcs.rs
+++ src/pkcs.rs
@@ -1,13 +1,8 @@
 pub fn pkcs7(bytes: &[u8], new_length: usize) -> Option<Vec<u8>> {
     let diff = new_length - bytes.len();
     if diff > 0 && diff < 256 {
-        let mut padded = vec![0; new_length];
-        for i in 0..bytes.len() {
-            padded[i] = bytes[i];
-        }
-        for i in bytes.len()..new_length {
-            padded[i] = diff as u8;
-        }
+        let mut padded = vec![diff as u8; new_length];
+        padded[..bytes.len()].clone_from_slice(bytes);
         Some(padded)
     } else {
         None
